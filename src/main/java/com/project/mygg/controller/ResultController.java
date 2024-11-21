7b74d525482b465b1d31213f35e565aefaa0191e
jayji5204 <jayji5204@naver.com>
2024-11-21 16:07:02 +0900

    resultPage 작성

--- src/main/java/com/project/mygg/controller/ResultController.java
+++ src/main/java/com/project/mygg/controller/ResultController.java
@@ -1,5 +1,8 @@
 package com.project.mygg.controller;
 
+import com.project.mygg.DTO.BoardDTO.BoardResponseDTO;
+import com.project.mygg.DTO.ReplyDTO.ReplyRequestDTO;
+import com.project.mygg.DTO.ReplyDTO.ReplyResponseDTO;
 import com.project.mygg.DTO.ResultDTO.ResultRequestDTO;
 import com.project.mygg.DTO.ResultDTO.ResultResponseDTO;
 import com.project.mygg.DTO.PlayerDTO.PlayerResponseDTO;
@@ -9,6 +12,7 @@ import com.project.mygg.service.PlayerService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -28,6 +32,15 @@ public class ResultController {
         Page<ResultResponseDTO> paging = resultService.findResult(page);
         model.addAttribute("paging", paging);
         return "/resultManage/gameResult";
+    }
+
+    @GetMapping("/gameResult/{id}")
+    public String getResult(@PathVariable Long id, Model model, @RequestParam(value = "page", defaultValue = "0") int page) {
+        ResultResponseDTO resultResponseDTO = resultService.getResult(id)
+                .orElseThrow(() -> new RuntimeException("경기결과를 찾을 수 없습니다."));
+        model.addAttribute("result", resultResponseDTO);
+
+        return "/resultManage/resultPage";
     }
 
     @GetMapping("/createResult")
--- src/main/java/com/project/mygg/service/ResultService.java
+++ src/main/java/com/project/mygg/service/ResultService.java
@@ -14,6 +14,8 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -34,5 +36,7 @@ public class ResultService {
     }
 
 
-
+    public Optional<ResultResponseDTO> getResult(Long id) {
+        return resultRepository.findById(id).map(ResultResponseDTO::new);
+    }
 }
